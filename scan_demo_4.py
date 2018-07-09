2ea1c38883b4c3540601f1ed854018fe41ad37d8
brucewu-fly <brucewu.fly.cn@gmail.com>
2018-07-09 20:05:04 +0800

    Add multiple thread demo

---
+++ scan_demo_4.py
@@ -0,0 +1,59 @@
+import time
+import sys
+import threading
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
+
+
+def slice_scan(size, slice_id, slice_max):
+    es = Elasticsearch()
+    cnt = 0
+    for _ in scan(es, query={"slice": {"id": slice_id, "max": slice_max}}, size=size, index=index):
+        cnt += 1
+        print cnt
+    print "sliced_id=%d, cnt=%d" % (slice_id, cnt)
+
+
+def do_run(size, slice_max):
+    t1 = time.time()
+    print "start time: %s" % t1
+
+    thread_lst = []
+    for i in range(slice_max):
+        t = threading.Thread(target=slice_scan, args=(size, i, slice_max))
+        thread_lst.append(t)
+
+    for t in thread_lst:
+        t.start()
+
+    for t in thread_lst:
+        t.join()
+
+    t2 = time.time()
+    print "end time: %s" % t2
+
+    diff = t2 - t1
+    print "cost: %ss" % diff
+    return diff
+
+
+def run(size, slice_max):
+    print "size: %d" % size
+    n = 3
+    total = 0
+    a = []
+    for i in range(n):
+        diff = do_run(size=size, slice_max=slice_max)
+        a.append(diff)
+        total += diff
+
+    avg = total / n
+    print a
+    print "avg: %ss" % avg
+
+
+index = sys.argv[1]
+arg1 = sys.argv[2]
+arg2 = sys.argv[3]
+run(int(arg1), int(arg2))
